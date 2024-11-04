b665b0b83b2ee81e0b2c6bda7524234c6fb1a191
gkwon <gkwon@student.42seoul.kr>
2024-11-05 01:43:50 +0900

    [refac]: rerange request limit

--- src/middleware/rateLimitMiddleware.js
+++ src/middleware/rateLimitMiddleware.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 const rateLimitMiddleware = rateLimit({
     windowMs: 15 * 60 * 1000,
-    limit: 150,
+    limit: 1500,
     message: 'Too many requests from this IP, please try again later',
     standardHeaders: true,
     legacyHeaders: false,
