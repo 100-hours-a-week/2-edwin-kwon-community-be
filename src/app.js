0d5e28bc1ae9a409ea97d0bcd36571997f2de3a7
gkwon <gkwon@student.42seoul.kr>
2024-12-19 14:53:51 +0900

    [refac]: fixed dotenv var error

--- src/app.js
+++ src/app.js
@@ -13,15 +13,19 @@ import {
 } from './middleware/securityMiddleware.js';
 import dbConnectionMiddleware from './middleware/dbConnection.js';
 import sessionConfig from './config/session.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const FRONTEND_URL = `http://${process.env.HOST}:3000`;
 
 const app = express();
 
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL, // 허용할 도메인 설정 (모든 도메인 허용 시 '*')
+        origin: `${FRONTEND_URL}`, // 허용할 도메인 설정 (모든 도메인 허용 시 '*')
         credentials: true, // 쿠키 전송을 위해 필요
     }),
 );
